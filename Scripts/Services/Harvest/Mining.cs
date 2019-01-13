760c0bfa87492f28bcc198a9c3ddb9b336bea5ce
raymaxquixunnn <35319979+raymaxquixunnn@users.noreply.github.com>
2019-01-13 09:19:09 +0900

    Revert "Revert "Link to resource skill""
    This reverts commit 490fa0f1b4531cda41fba001fad5c14da4123000.

--- Scripts/Services/Harvest/Mining.cs
+++ Scripts/Services/Harvest/Mining.cs
@@ -93,26 +93,38 @@ namespace Server.Engines.Harvest
             {
                 new HarvestResource(00.0, 00.0, 100.0, 1007072, typeof(IronOre), typeof(Granite)),
                 new HarvestResource(65.0, 25.0, 105.0, 1007073, typeof(DullCopperOre),	typeof(DullCopperGranite), typeof(DullCopperElemental)),
-                new HarvestResource(70.0, 30.0, 110.0, 1007074, typeof(ShadowIronOre),	typeof(ShadowIronGranite), typeof(ShadowIronElemental)),
-                new HarvestResource(75.0, 35.0, 115.0, 1007075, typeof(CopperOre), typeof(CopperGranite), typeof(CopperElemental)),
-                new HarvestResource(80.0, 40.0, 120.0, 1007076, typeof(BronzeOre), typeof(BronzeGranite), typeof(BronzeElemental)),
-                new HarvestResource(85.0, 45.0, 125.0, 1007077, typeof(GoldOre), typeof(GoldGranite), typeof(GoldenElemental)),
-                new HarvestResource(90.0, 50.0, 130.0, 1007078, typeof(AgapiteOre), typeof(AgapiteGranite), typeof(AgapiteElemental)),
-                new HarvestResource(95.0, 55.0, 135.0, 1007079, typeof(VeriteOre), typeof(VeriteGranite), typeof(VeriteElemental)),
-                new HarvestResource(99.0, 59.0, 139.0, 1007080, typeof(ValoriteOre), typeof(ValoriteGranite), typeof(ValoriteElemental))
+                new HarvestResource(80.0, 40.0, 110.0, 1007074, typeof(ShadowIronOre),	typeof(ShadowIronGranite), typeof(ShadowIronElemental)),
+                new HarvestResource(95.0, 55.0, 115.0, 1007075, typeof(CopperOre), typeof(CopperGranite), typeof(CopperElemental)),
+                new HarvestResource(100.0, 60.0, 120.0, 1007076, typeof(BronzeOre), typeof(BronzeGranite), typeof(BronzeElemental)),
+                new HarvestResource(105.0, 65.0, 125.0, 1007077, typeof(GoldOre), typeof(GoldGranite), typeof(GoldenElemental)),
+                new HarvestResource(110.0, 70.0, 125.0, 1007077, typeof(SilverOre)),
+                new HarvestResource(115.0, 75.0, 130.0, 1007078, typeof(AgapiteOre), typeof(AgapiteGranite), typeof(AgapiteElemental)),
+                new HarvestResource(120.0, 80.0, 135.0, 1007079, typeof(VeriteOre), typeof(VeriteGranite), typeof(VeriteElemental)),
+                new HarvestResource(125.0, 80.0, 139.0, 1007080, typeof(ValoriteOre), typeof(ValoriteGranite), typeof(ValoriteElemental))
             };
 
             veins = new HarvestVein[]
             {
-                new HarvestVein(49.6, 0.0, res[0], null), // Iron
-                new HarvestVein(11.2, 0.5, res[1], res[0]), // Dull Copper
-                new HarvestVein(09.8, 0.5, res[2], res[0]), // Shadow Iron
-                new HarvestVein(08.4, 0.5, res[3], res[0]), // Copper
-                new HarvestVein(07.0, 0.5, res[4], res[0]), // Bronze
-                new HarvestVein(05.6, 0.5, res[5], res[0]), // Gold
-                new HarvestVein(04.2, 0.5, res[6], res[0]), // Agapite
-                new HarvestVein(02.8, 0.5, res[7], res[0]), // Verite
-                new HarvestVein(01.4, 0.5, res[8], res[0])// Valorite
+				new HarvestVein(50.0000, 0.0, res[0], null),
+				new HarvestVein(25.0000, 0.5, res[1], res[0]),
+				new HarvestVein(12.5000, 0.5, res[2], res[0]),
+				new HarvestVein(06.2500, 0.5, res[3], res[0]),
+				new HarvestVein(03.1250, 0.5, res[4], res[0]),
+				new HarvestVein(01.5625, 0.5, res[5], res[0]),
+				new HarvestVein(00.7813, 0.5, res[6], res[0]),
+				new HarvestVein(00.3906, 0.5, res[7], res[0]),
+				new HarvestVein(00.1953, 0.5, res[8], res[0]),
+				new HarvestVein(00.0977, 0.5, res[9], res[0]),
+				new HarvestVein(00.0488, 0.5, res[10], res[0]),
+				new HarvestVein(00.0244, 0.5, res[11], res[0]),
+				new HarvestVein(00.0122, 0.5, res[12], res[0]),
+				new HarvestVein(00.0061, 0.5, res[13], res[0]),
+				new HarvestVein(00.0031, 0.5, res[14], res[0]),
+				new HarvestVein(00.0015, 0.5, res[15], res[0]),
+				new HarvestVein(00.0008, 0.5, res[16], res[0]),
+				new HarvestVein(00.0004, 0.5, res[17], res[0]),
+				new HarvestVein(00.0002, 0.5, res[18], res[0]),
+				new HarvestVein(00.0001, 0.5, res[19], res[0]),
             };
 
             oreAndStone.Resources = res;
